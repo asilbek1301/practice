b4a72436b8addcb01eac9012f87bcab5c154a8a1
asilbek <asilbekotanazarov05@dhu.ac.kr>
2026-04-16 23:10:20 +0900

    feat: learn python boolean

--- primitive.py
+++ primitive.py
@@ -1,6 +1,6 @@
 print("====== number ======")
 # in JAVA, variable is a name storage location!
-# in Python, variable is named reference!
+# in Python, variable is named reference! (referensning nomlanishi)
 
 count = 100
 count_type = type(count)
@@ -16,13 +16,31 @@ print("====== string ======")
 course = "AI Python FullStack"
 
 result = type(course)
-print(f"the result (1): {result}")  
+print(f"the result (1): {result}")
 
 result = course.title()
-print(f"the result (2): {result}")  
+print(f"the result (2): {result}")
 
 result = course.upper()
 print(f"the result (3): {result}")
 
 result = course.replace("FullStack", "MasterClass")
 print(f"the result (4): {result}")
+
+print("====== boolean ======")
+# functions > type() inout() bool() int() str()
+y = input("GIve your value for y: ")
+print("y:", y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# TRUTHY vs FALSY value
+# TRUTHY: True 100 -100 "MIT"
+# FALSY: False 0 "" None
+
+test_falsy = "" or False or None or 0
+print("test_falsy:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("test_truthy:", bool(test_truthy))

02843f7b447c5478685fd78a534b6ef390cf13e7
asilbek <asilbekotanazarov05@dhu.ac.kr>
2026-05-06 14:18:16 +0900

    fix: I-TASK SOLUTION

--- train.py
+++ train.py
@@ -1,3 +1,28 @@
+''' I-TASK (PYTHON): [2026년 5월 6일]
+⭐️  Savol: Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin. 
+MASALAN: get_digits("m14i1t") return qiladi "141"
+'''
+
+# ⭐️  Masalaning yechimi:
+# DEFINE
+
+
+def get_digits(string):
+    digits = ""
+    for letter in string:
+        if letter.isnumeric():
+            digits += letter
+    return digits
+
+
+# CALL
+result = get_digits("m14i1t")
+print(f"result: {result}")
+
+
+# ==================================================================
+
+
 ''' G-TASK (PYTHON): [2026년 4월 30일]
 ⭐️  Savol: Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin. 
 MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini.
@@ -6,7 +31,7 @@ MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 # ⭐️  Masalaning yechimi:
 # DEFINE
 
-
+'''
 def get_highest_index(numb_list):
     highest_index = 0
     for number in numb_list:
@@ -18,3 +43,4 @@ def get_highest_index(numb_list):
 # CALL
 result = get_highest_index([5, 21, 12, 21, 8])
 print(f"result: {result}")
+'''
